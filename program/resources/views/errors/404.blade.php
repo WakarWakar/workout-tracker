bf215120c25510f761a9ab39b63b3fd1eddfac10
WakarWakar <vakarish@duck.com>
2026-06-14 17:34:23 +0200

    feat: Implement 404 error handling, hide unauthorized pages, and update security design choices in README

---
+++ program/resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Not Found - Workout Tracker</title>
+</head>
+<body>
+
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you're looking for doesn't exist or may have been moved.</p>
+    <p><a href="/">Go back to home</a></p>
+
+</body>
+</html>
