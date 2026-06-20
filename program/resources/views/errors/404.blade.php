e5a4851288fc7b292e6d2fcc80dbb4b97e377279
WakarWakar <vakarish@duck.com>
2026-06-20 23:16:35 +0200

    refactor: Update 404 error page to redirect admins to /admin and others to home

--- program/resources/views/errors/404.blade.php
+++ program/resources/views/errors/404.blade.php
@@ -9,7 +9,8 @@
 
     <h1>404 - Page Not Found</h1>
     <p>Sorry, the page you're looking for doesn't exist, may have been moved, or requires logging in.</p>
-    <p><a href="/">Go back to home</a></p>
+    @php($home = auth()->check() && auth()->user()->isAdmin() ? '/admin' : '/')
+    <p><a href="{{ $home }}">Go back to home</a></p>
 
 </body>
 </html>
